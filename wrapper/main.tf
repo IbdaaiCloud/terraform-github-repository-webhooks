cf88d2a87fda3988f63c83f8eb4ce9c6ab4c8a2c
MKAbuMattar <mohammad.khaled@outlook.com>
2025-01-26 19:47:44 +0300

    fix!: rename repository_webhook_enabled to enable_repository_webhook_creation for clarity

--- wrapper/main.tf
+++ wrapper/main.tf
@@ -3,7 +3,7 @@ module "wrapper" {
 
   for_each = var.items
 
-  repository_webhook_enabled          = try(each.value.repository_webhook_enabled, var.defaults.repository_webhook_enabled, null)
+  enable_repository_webhook_creation  = try(each.value.enable_repository_webhook_creation, var.defaults.enable_repository_webhook_creation, null)
   repository_workflow_repository_name = try(each.value.repository_workflow_repository_name, var.defaults.repository_workflow_repository_name, null)
   repository_webhook_events           = try(each.value.repository_webhook_events, var.defaults.repository_webhook_events, null)
   repository_webhook_active           = try(each.value.repository_webhook_active, var.defaults.repository_webhook_active, null)
